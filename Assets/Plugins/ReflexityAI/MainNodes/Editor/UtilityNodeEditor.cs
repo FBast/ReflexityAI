61b132b2ccdc61b8c7d64cdf199336a2e96d5ef7
Dizy <bast.frederic@gmail.com>
2020-04-30 15:09:51 +0200

    Refactored namespaces

--- Assets/Plugins/ReflexityAI/MainNodes/Editor/UtilityNodeEditor.cs
+++ Assets/Plugins/ReflexityAI/MainNodes/Editor/UtilityNodeEditor.cs
@@ -12,7 +12,7 @@ namespace Plugins.ReflexityAI.MainNodes.Editor {
             if (_utilityNode == null) _utilityNode = (UtilityNode) target;
             serializedObject.Update();
             NodeEditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(_utilityNode.MinX)));
-            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(_utilityNode.X	)));
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(_utilityNode.X)));
             NodeEditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(_utilityNode.MaxX)));
             EditorGUILayout.CurveField(GUIContent.none, _utilityNode.Function, GUILayout.Height(50));
 //            EditorGUILayout.CurveField(GUIContent.none, _utilityNode.ScaledFunction, GUILayout.Height(50));
